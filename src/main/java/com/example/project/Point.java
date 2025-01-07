dbc50a422457793beff9c272b9349f1d187f759a
KoeruLin <167535514+KoeruLin@users.noreply.github.com>
2025-01-07 02:44:59 +0000

    done

--- src/main/java/com/example/project/Point.java
+++ src/main/java/com/example/project/Point.java
@@ -1,22 +1,41 @@
 package com.example.project;
 
-public class Point {
-    private int x;
-    private int y;
-    
-    public Point(int x, int y) {
-      this.x = x;
-      this.y = y;
-    }
-    
-    // Returns distance between this Point object and another Point object
-    public double distanceTo(Point other) {
-      /* IMPLEMENT ME */
-    }
-  
-    // Returns a string in the format: (x, y)
-    public String pointInfo() {
-      /* IMPLEMENT ME */
-    }
-  }
-  
+class Point
+{
+   private final int x;
+   private final int y;
+
+
+   public Point(int x, int y)
+   {
+       this.x = x;
+       this.y = y;
+   }
+
+
+   public int getX()
+   {
+       return x;
+   }
+
+
+   public int getY()
+   {
+       return y;
+   }
+
+
+   // Returns distance between this Point object and another Point object
+   public double distanceTo(Point other)
+   {
+       double distance = Math.pow(other.getX() - this.getX(), 2) + Math.pow(other.getY() - this.getY(), 2);
+       return Math.sqrt(distance);
+   }
+
+
+   // Returns a string in the format: (x, y)
+   public String pointInfo()
+   {
+       return "(" + x + ", " + y + ")";
+   }
+}
--- src/main/java/com/example/project/Triangle.java
+++ src/main/java/com/example/project/Triangle.java
@@ -1,26 +1,50 @@
 package com.example.project;
 
-public class Triangle {
-    // array of Point objects
-    private Point[] vertices;
-    
-    // Constructor: initializes the vertices array
-    // to contain the three Point objects
-    public Triangle(Point p1, Point p2, Point p3) {
-      /* IMPLEMENT ME */
-    }
-    
-    // Returns the perimeter of the Triangle
-    // HINT: use the distanceTo method that you wrote in the Point class
-    public double perimeter() {
-      /* IMPLEMENT ME */
-    }
-  
-    // Returns a String with the three vertices that make up the Triangle;
-    // if the vertices are the points (6, 10), (11, 15), and (18, 7),
-    // this method should return: "[(6, 10), (11, 15), (18, 7)]"
-    public String triangleInfo() {
-      /* IMPLEMENT ME */
-    }
-  }
-  
+class Triangle
+{
+   // array of Point objects
+   private final Point[] vertices = new Point[3];
+
+
+   // Constructor: initializes the vertices array
+   // to contain the three Point objects
+   public Triangle(Point p1, Point p2, Point p3)
+   {
+       vertices[0] = p1;
+       vertices[1] = p2;
+       vertices[2] = p3;
+   }
+
+
+   public Point[] getVertices()
+   {
+       return vertices;
+   }
+
+
+   // Returns the perimeter of the Triangle
+   // HINT: use the distanceTo method that you wrote in the Point class
+   public double perimeter()
+   {
+       double sideOne = vertices[1].distanceTo(vertices[0]);
+       double sideTwo = vertices[1].distanceTo(vertices[2]);
+       double sideThree = vertices[2].distanceTo(vertices[0]);
+
+
+       return sideOne + sideTwo + sideThree;
+   }
+
+
+   // Returns a String with the three vertices that make up the Triangle;
+   // if the vertices are the points (6, 10), (11, 15), and (18, 7),
+   // this method should return: "[(6, 10), (11, 15), (18, 7)]"
+   public String triangleInfo()
+   {
+       String pointOne = vertices[0].pointInfo();
+       String pointTwo = vertices[1].pointInfo();
+       String pointThree = vertices[2].pointInfo();
+
+
+       return "[" + pointOne + ", " + pointTwo + ", " + pointThree + "]";
+   }
+}
